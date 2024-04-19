4860a030e77683164fdcedb62afdab9cb7cb49de
joaojcfr <joao.ferreira95@yahoo.com.br>
2024-04-19 19:39:45 +0000

    First commit

--- schoolSystem.js
+++ schoolSystem.js
@@ -1,79 +1,38 @@
-const schoolSystem = {
-  schools: [
-    {
-      name: "Fake School 1",
-      classRooms: [
-        {
-          teacher: { firstName: "Marcelino", lastName: "Padberg", age: 25 },
-          students: [
-            { firstName: "Aliyah", lastName: "Schulist", age: 18 },
-            { firstName: "Cleveland", lastName: "Towne", age: 28 },
-            { firstName: "Jan", lastName: "Quitzon", age: 18 },
-            { firstName: "Alaina", lastName: "Runolfsdottir", age: 18 },
-            { firstName: "Gerhard", lastName: "Bergstrom", age: 23 },
-          ],
-        },
-        {
-          teacher: { firstName: "Edwardo", lastName: "Schowalter", age: 28 },
-          students: [
-            { firstName: "Manley", lastName: "Doyle", age: 18 },
-            { firstName: "Maximilian", lastName: "Gleichner", age: 19 },
-            { firstName: "Sid", lastName: "Rohan", age: 30 },
-            { firstName: "Catalina", lastName: "Hilpert", age: 27 },
-            { firstName: "Gerald", lastName: "O'Keefe", age: 26 },
-          ],
-        },
-      ],
-    },
-    {
-      name: "Fake School 2",
-      classRooms: [
-        {
-          teacher: { firstName: "Lucas", lastName: "Schroeder", age: 29 },
-          students: [
-            { firstName: "Giuseppe", lastName: "Hegmann", age: 24 },
-            { firstName: "Jennyfer", lastName: "Hane", age: 19 },
-            { firstName: "Mikayla", lastName: "Braun", age: 23 },
-            { firstName: "Rickie", lastName: "White", age: 22 },
-            { firstName: "Rose", lastName: "Collins", age: 30 },
-          ],
-        },
-        {
-          teacher: { firstName: "Green", lastName: "Sauer", age: 25 },
-          students: [
-            { firstName: "Melany", lastName: "Welch", age: 25 },
-            { firstName: "Paxton", lastName: "Corkery", age: 22 },
-            { firstName: "Nellie", lastName: "Hauck", age: 26 },
-            { firstName: "Eunice", lastName: "Hirthe", age: 26 },
-            { firstName: "Aylin", lastName: "Barrows", age: 26 },
-          ],
-        },
-      ],
-    },
-    {
-      name: "Fake School 3",
-      classRooms: [
-        {
-          teacher: { firstName: "Nikko", lastName: "Crist", age: 42 },
-          students: [
-            { firstName: "Christop", lastName: "Hahn", age: 26 },
-            { firstName: "Newell", lastName: "Kemmer", age: 27 },
-            { firstName: "Katheryn", lastName: "Heller", age: 26 },
-            { firstName: "Saul", lastName: "Heathcote", age: 20 },
-            { firstName: "Maudie", lastName: "Haley", age: 30 },
-          ],
-        },
-        {
-          teacher: { firstName: "Nathanael", lastName: "Hansson", age: 50 },
-          students: [
-            { firstName: "Jensen", lastName: "Reichel", age: 21 },
-            { firstName: "Lois", lastName: "Kulas", age: 28 },
-            { firstName: "Caterina", lastName: "Wolff", age: 28 },
-            { firstName: "Dahlia", lastName: "Collier", age: 24 },
-            { firstName: "Linwood", lastName: "Langosh", age: 26 },
-          ],
-        },
-      ],
-    },
-  ],
-};
+/*
+function someNames(firstName, lastName){
+  let results = `Hello, my first name is ${firstName.length} and my last name is ${lastName}`
+  let a = 1
+  let b = 2
+  return a + b
+}
+
+const func = (Input) => Input *5;
+
+*/
+
+let narNumber = 8208
+
+function calcFunc(narNumb){
+  let numStr = narNumb.toString()
+  let power = numStr.length
+  let splitString = numStr.split("");
+
+  let myArray = splitString.map(element => parseInt(element))
+  let finalNumber = myArray.map(element => element ** power)
+
+  let sum = 0
+  for(let i = 0; i < myArray.length; i = i +1){
+    sum = sum + finalNumber[i];
+  }
+
+  if(sum == narNumb){
+    return "Narcisistic"
+  }else{
+    return "Non-Narcisistic"
+  }
+
+  
+}
+
+
+console.log(calcFunc(narNumber));
